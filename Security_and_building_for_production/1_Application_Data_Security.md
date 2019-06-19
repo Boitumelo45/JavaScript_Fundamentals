48fe17af4e6cfc6f30235b1c582c3e55964acfe8
Boitumelo45 <b.phetla@icloud.com>
2019-06-19 22:56:58 +0200

    Security and Building for Production

---
+++ Security_and_building_for_production/1_Application_Data_Security.md
@@ -0,0 +1,8 @@
+
+Don't store passwords, secrets or other sensitive information
+
+Don't use global variables
+
+Assume hackers can read your JavaScript code and access all the data sent to a browser
+
+Use JavaScript Obfuscator to hide/or make your <JS></JS> code more complicated
---
+++ Security_and_building_for_production/2_Security_and_eval_function.js
@@ -0,0 +1,35 @@
+
+the eval string:
+
+    let someString = "console.log("this is a string");"
+
+    eval(someString) //dynamic programming
+
+    this can be used to execute code by a hacker from an injection code keyword
+    like changing console.log() to alert()
+
+
+Prevent man in the middle Attack:
+
+    Use SSL
+
+    Use HTTP Header: Strict-Transport-Security
+
+    Use cookie attributes: Secure and HttpOnly
+
+
+Cross Site Scripting Attacks:
+
+        XSS
+
+        to prevent XSS:
+
+            CSP: Content Security Policy
+
+            CORS: Cross Origin Resource Sharing
+
+How to build applications for Production:
+
+        webpack
+
+        
